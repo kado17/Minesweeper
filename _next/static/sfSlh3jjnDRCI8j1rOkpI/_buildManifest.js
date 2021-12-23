cc85e24512c3f1422d883a3db4993765a3d43814
s1f102100119 <s1f102100119@users.noreply.github.com>
2021-12-23 16:13:53 +0000

    deploy: 2791b81dcf466cf2e2d43e810456c1e6a3de804c

---
+++ _next/static/sfSlh3jjnDRCI8j1rOkpI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/163-c98103972be1d7024d11.js","static/chunks/pages/index-0e2d3103e0b37cd60773.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
