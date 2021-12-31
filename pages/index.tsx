4fd6be98a0b9fee19cdfae1657cf5b2fadda5e6e
s1f102100119 <s1f102100119@iniad.org>
2021-12-31 21:08:49 +0900

    コードの整理及びcssの設定変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const IMAGE = 'images/img.png'
+//Blocksize >> 5vh
 
 const Container = styled.div`
   display: flex;
@@ -11,123 +12,111 @@ const Container = styled.div`
   height: 100vh;
   background-color: #008080;
 `
-const Board = styled.div`
+const Board = styled.div<{ numberOfBlocks: { width: number; height: number } }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
-  width: 60vh;
-  height: 80vh;
+  width: ${(props) => props.numberOfBlocks.width * 5 + 5}vh;
+  height: ${(props) => props.numberOfBlocks.height * 5 + 19}vh;
   background-color: #ccc;
   border: 0.8vh solid;
   border-color: #ddd #666 #666 #ddd;
 `
-const StateBoard = styled.div`
+const StateBoard = styled.div<{ widthNumberOfBlocks: number }>`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-around;
-  width: 54vh;
-  height: 12vh;
+  justify-content: space-between;
+  width: ${(props) => props.widthNumberOfBlocks * 5 + 2}vh;
+  height: 11vh;
   border: 0.7vh solid;
   border-color: #666 #ddd #ddd #666;
 `
 
-const CounterTemplate = styled.div`
+const Flagcouner = styled.div`
   display: inline;
-  width: 16vh;
-  height: 10vh;
-  font-size: 9vh;
-  line-height: 8.5vh;
+  width: 13vh;
+  height: 8vh;
+  margin-left: 1vh;
+  font-size: 7vh;
+  line-height: 6.5vh;
   color: red;
   text-align: center;
   background-color: black;
   border: 0.5vh solid black;
   border-color: #666 #ddd #ddd #666;
 `
-const Flagcouner = styled(CounterTemplate)``
 
-const CountUpTimer = styled(CounterTemplate)``
+const CountUpTimer = styled(Flagcouner)`
+  margin-right: 1vh;
+`
 
-const Face = styled.div<{ faceState: string }>`
-  width: 10vh;
-  height: 10vh;
+const Face = styled.div<{ faceState: number }>`
+  width: 8vh;
+  height: 8vh;
   background-image: url(${IMAGE});
   background-repeat: no-repeat;
-  background-position: ${(props) => props.faceState} 0;
+  background-position: ${(props) => props.faceState * -8}vh 0;
   background-origin: border-box;
-  background-size: 140vh 10vh;
+  background-size: 112vh 8vh;
   border: 0.6vh solid;
   border-color: #ddd #666 #666 #ddd;
 `
 
-const GameBoard = styled.div`
-  width: 56vh;
-  height: 56vh;
+const GameBoard = styled.div<{ numberOfBlocks: { width: number; height: number } }>`
+  width: ${(props) => props.numberOfBlocks.width * 5 + 2}vh;
+  height: ${(props) => props.numberOfBlocks.height * 5 + 2}vh;
   border: 1vh solid;
   border-color: #666 #ddd #ddd #666;
 `
 
-const BlockTemplate = styled.div`
+const BombBlock = styled.div`
   display: inline-block;
-  width: 6vh;
-  height: 6vh;
+  width: 5vh;
+  height: 5vh;
   vertical-align: bottom;
-`
-
-const GameBlock = styled(BlockTemplate)<{ isOpen: boolean; imagePosition: string }>`
-  background-color: ${(props) => (props.isOpen ? 'white' : 'gray')};
+  background-color: white;
   background-image: url(${IMAGE});
   background-repeat: no-repeat;
-  background-position: ${(props) => props.imagePosition} 0;
-  background-size: 84vh 6vh;
+  background-position: -50vh 0;
+  background-size: 70vh 5vh;
+  border: 0.1vh solid #666;
+`
+
+const GameBlock = styled(BombBlock)<{ isOpen: boolean; num: number }>`
+  background-color: ${(props) => (props.isOpen ? 'white' : 'gray')};
+  background-position: ${(props) => -5 * (props.num - 1) - 0.1}vh 0;
   ${(props) =>
     props.isOpen
       ? 'border: 0.1vh solid #666;'
       : 'border: 0.4vh solid;' + 'border-color: #bbb #666 #666 #bbb;'}
 `
-
-const BombBlock = styled(BlockTemplate)`
-  background-color: white;
-  background-image: url(${IMAGE});
-  background-repeat: no-repeat;
-  background-position: -60vh 0;
-  background-size: 84vh 6vh;
-  border: 0.1vh solid #666;
-`
-
-const FlagBlock = styled(BlockTemplate)<{ imagePosition: string }>`
-  background: gray;
-  background-image: url(${IMAGE});
-  background-repeat: no-repeat;
-  background-position: ${(props) => props.imagePosition} -0.3vh;
-  background-origin: padding-box;
-  background-size: 84vh 6vh;
+const FlagBlock = styled(BombBlock)<{ num: number }>`
+  background-color: gray;
+  background-position: ${(props) => -5 * (props.num - 3) - 0.1}vh -0.3vh;
   border: 0.3vh solid;
   border-color: #bbb #666 #666 #bbb;
 `
 
 const Home: NextPage = () => {
   //フィールドの大きさと爆弾の数の定義
-  const gameField = { firldSize: 9, numberOfBombs: 10 }
-  // prettier-ignore
-  const startBoard = [
-    [9, 9, 9, 9, 9, 9, 9, 9, 9],
-    [9, 9, 9, 9, 9, 9, 9, 9, 9],
-    [9, 9, 9, 9, 9, 9, 9, 9, 9],
-    [9, 9, 9, 9, 9, 9, 9, 9, 9],
-    [9, 9, 9, 9, 9, 9, 9, 9, 9],
-    [9, 9, 9, 9, 9, 9, 9, 9, 9],
-    [9, 9, 9, 9, 9, 9, 9, 9, 9],
-    [9, 9, 9, 9, 9, 9, 9, 9, 9],
-    [9, 9, 9, 9, 9, 9, 9, 9, 9]
-  ]
+
   const startBombs: { x: number; y: number }[] = []
   const startState = { isGameclear: false, isGameover: false }
+  const [gameConfig, setGameConfig] = useState({
+    widthBlocks: 30,
+    heightBlocks: 16,
+    numberOfBombs: 10,
+  })
+  const startBoard = Array.from(new Array(gameConfig.heightBlocks), () =>
+    new Array(gameConfig.widthBlocks).fill(9)
+  )
+  console.log(startBoard)
   const [board, setBoard] = useState(startBoard)
   const [bombs, setBombs] = useState(startBombs)
   const [gameState, setGameState] = useState(startState)
-  const [flagCount, setFlagCount] = useState(gameField.numberOfBombs)
+  const [flagCount, setFlagCount] = useState(gameConfig.numberOfBombs)
   const [timer, setTimer] = useState(0)
 
   useEffect(() => {
@@ -146,9 +135,9 @@ const Home: NextPage = () => {
     //爆弾の生成関数
     const createBomb = (x: number, y: number) => {
       const tmpBombs: { x: number; y: number }[] = []
-      while (tmpBombs.length < gameField.numberOfBombs) {
-        const randomX = Math.floor(Math.random() * gameField.firldSize)
-        const randomY = Math.floor(Math.random() * gameField.firldSize)
+      while (tmpBombs.length < gameConfig.numberOfBombs) {
+        const randomX = Math.floor(Math.random() * gameConfig.widthBlocks)
+        const randomY = Math.floor(Math.random() * gameConfig.heightBlocks)
         if (
           !tmpBombs.some((b) => b.x === randomX && b.y === randomY) &&
           x !== randomX &&
@@ -166,9 +155,9 @@ const Home: NextPage = () => {
         for (let yi = y - 1; yi < y + 2; yi++) {
           if (
             0 <= xi &&
-            xi < gameField.firldSize &&
+            xi < gameConfig.widthBlocks &&
             0 <= yi &&
-            yi < gameField.firldSize &&
+            yi < gameConfig.heightBlocks &&
             { x: x, y: y } !== { x: xi, y: yi }
           ) {
             result.push({ x: xi, y: yi })
@@ -244,7 +233,7 @@ const Home: NextPage = () => {
         notOpenBlockCount += row.filter((num) => num === 9 || 11 <= num).length
       }
       //勝利判定
-      if (notOpenBlockCount === gameField.numberOfBombs) {
+      if (notOpenBlockCount === gameConfig.numberOfBombs) {
         setGameState({ ...gameState, isGameclear: true })
         for (const bom of newBombs) {
           newBoard[bom.y][bom.x] = 12
@@ -282,15 +271,15 @@ const Home: NextPage = () => {
     setBoard(startBoard)
     setBombs(startBombs)
     setGameState(startState)
-    setFlagCount(gameField.numberOfBombs)
+    setFlagCount(gameConfig.numberOfBombs)
     setTimer(0)
   }
 
   const cheat = (e: React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault()
     const newBoard: number[][] = JSON.parse(JSON.stringify(board))
-    for (let x = 0; x < gameField.firldSize; x++) {
-      for (let y = 0; y < gameField.firldSize; y++) {
+    for (let x = 0; x < gameConfig.widthBlocks; x++) {
+      for (let y = 0; y < gameConfig.heightBlocks; y++) {
         if (!bombs.some((b) => b.x === x && b.y === y)) {
           newBoard[y][x] = 0
         }
@@ -303,7 +292,7 @@ const Home: NextPage = () => {
       notOpenBlockCount += row.filter((num) => num === 9 || 11 <= num).length
     }
     //勝利判定
-    if (notOpenBlockCount === gameField.numberOfBombs) {
+    if (notOpenBlockCount === gameConfig.numberOfBombs) {
       setGameState({ ...gameState, isGameclear: true })
       for (const bom of bombs) {
         newBoard[bom.y][bom.x] = 12
@@ -312,21 +301,20 @@ const Home: NextPage = () => {
   }
   return (
     <Container>
-      <Board>
-        <StateBoard>
+      <Board numberOfBlocks={{ width: gameConfig.widthBlocks, height: gameConfig.heightBlocks }}>
+        <StateBoard widthNumberOfBlocks={gameConfig.widthBlocks}>
           <Flagcouner>{('000' + flagCount).slice(-3)}</Flagcouner>
           <Face
-            faceState={
-              ((gameState.isGameover ? 13 : gameState.isGameclear ? 12 : 11) * -10).toString() +
-              'vh'
-            }
+            faceState={gameState.isGameover ? 13 : gameState.isGameclear ? 12 : 11}
             onClick={() => reset()}
             onContextMenu={(e) => cheat(e)}
           ></Face>
           <CountUpTimer>{timer > 999 ? 999 : ('000' + timer).slice(-3)}</CountUpTimer>
         </StateBoard>
 
-        <GameBoard>
+        <GameBoard
+          numberOfBlocks={{ width: gameConfig.widthBlocks, height: gameConfig.heightBlocks }}
+        >
           {board.map((row, y) =>
             row.map((num, x) =>
               num === 10 ? (
@@ -334,17 +322,14 @@ const Home: NextPage = () => {
               ) : 11 <= num ? (
                 <FlagBlock
                   key={`${x}-${y}`}
-                  imagePosition={(-6 * (num - 3) - 0.1).toString() + 'vh'}
+                  num={num}
                   onContextMenu={(e) => rightClick(x, y, e)}
                 ></FlagBlock>
               ) : (
                 <GameBlock
                   key={`${x}-${y}`}
                   isOpen={num < 9}
-                  imagePosition={
-                    1 <= num && num <= 8 ? (-6 * (num - 1) - 0.1).toString() + 'vh' : '100vh'
-                    //100vhは適当な値
-                  }
+                  num={1 <= num && num <= 8 ? num : 100} //100は適当な値
                   onClick={() => onClick(x, y)}
                   onContextMenu={(e) => rightClick(x, y, e)}
                 ></GameBlock>
